dbfa31694c1352459bb512e2bbddc456b53fdeba
Alberto <Alberto.delangel3@Chubb.com>
2022-02-10 13:35:57 -0600

    Modificacion de codigo github

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(735, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(41, 107);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(114, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Busqueda RegExp";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 136);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(802, 418);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(696, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Selecciona carpeta:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ruta seleccionada:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 556);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "SSIS Buscador";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string m_strFolder = string.Empty;
+        private DataTable m_lst = new DataTable();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            fbd.ShowDialog();
+            m_strFolder = fbd.SelectedPath;
+            textBox1.Text = m_strFolder;
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(m_strFolder))
+            {
+                if (!radioButton1.Checked) {
+                    MessageBox.Show("You have to select a RadioBox");
+                }
+                else
+                {
+                    ReadPackagesInfo(m_strFolder);
+                }
+                dataGridView1.DataSource = m_lst;
+                dataGridView1.Refresh();
+            }
+            else{
+                MessageBox.Show("You have to select a Source Folder");
+            }
+        }
+
+        private void ReadPackagesInfo(string strDirectory)
+        {
+            m_lst.Clear();
+            m_lst.Columns.Add("PackageFileName", typeof(string));
+            m_lst.Columns.Add("PackageFormatVersion", typeof(string));
+            m_lst.Columns.Add("CreationDate", typeof(string));
+            m_lst.Columns.Add("CreationName", typeof(string));
+            m_lst.Columns.Add("CreatorComputerName", typeof(string));
+            m_lst.Columns.Add("DTSID", typeof(string));
+            m_lst.Columns.Add("ExecutableType", typeof(string));
+            m_lst.Columns.Add("LastModifiedProductVersion", typeof(string));
+            m_lst.Columns.Add("LocaleID", typeof(string));
+            m_lst.Columns.Add("ObjectName", typeof(string));
+            m_lst.Columns.Add("PackageType", typeof(string));
+            m_lst.Columns.Add("VersionBuild", typeof(string));
+            m_lst.Columns.Add("VersionGUID", typeof(string));
+
+            foreach (var strFile in System.IO.Directory.GetFiles(strDirectory, "*.dtsx", System.IO.SearchOption.AllDirectories))
+            {
+                string strContent = "";
+
+                StreamReader sr = new StreamReader(strFile);
+                strContent = sr.ReadToEnd();
+                sr.Close();
+
+                string strPackageFormatVersion = Regex.Match(strContent, @"(?<=""PackageFormatVersion"">)(.*)(?=</DTS:Property>)", RegexOptions.Singleline).Value;
+                string strCreationDate = Regex.Match(strContent, @"(?<=DTS:CreationDate="")(.*?)(?="")", RegexOptions.Singleline).Value;
+                string strCreationName = Regex.Match(strContent, @"(?<=DTS:CreationName="")(.*?)(?="")", RegexOptions.Singleline).Value;
+                string strCreatorComputerName = Regex.Match(strContent, @"(?<=DTS:CreatorComputerName="")(.*?)(?="")", RegexOptions.Singleline).Value;
+                string strCreatorName = Regex.Match(strContent, @"(?<=DTS:CreatorName"">)(.*)(?=</DTS:Property>)", RegexOptions.Singleline).Value;
+                string strDTSID = Regex.Match(strContent, @"(?<=DTS:DTSID="")(.*?)(?="")", RegexOptions.Singleline).Value;
+                string strExecutableType = Regex.Match(strContent, @"(?<=DTS:ExecutableType="")(.*?)(?="")", RegexOptions.Singleline).Value;
+                string strLastModifiedProductVersion = Regex.Match(strContent, @"(?<=DTS:LastModifiedProductVersion="")(.*?)(?="")", RegexOptions.Singleline).Value;
+                string strLocaleID = Regex.Match(strContent, @"(?<=DTS:LocaleID="")(.*?)(?="")", RegexOptions.Singleline).Value;
+                string strObjectName = Regex.Match(strContent, @"(?<=DTS:ObjectName="")(.*?)(?="")", RegexOptions.Singleline).Value;
+                string strPackageType = Regex.Match(strContent, @"(?<=DTS:PackageType="")(.*?)(?="")", RegexOptions.Singleline).Value;
+                string strVersionBuild = Regex.Match(strContent, @"(?<=DTS:VersionBuild="")(.*?)(?="")", RegexOptions.Singleline).Value;
+                string strVersionGUID = Regex.Match(strContent, @"(?<=DTS:VersionGUID="")(.*?)(?="")", RegexOptions.Singleline).Value;
+
+
+
+                m_lst.Rows.Add(strFile,
+                    strPackageFormatVersion, strCreationDate, strCreationName,
+                    strCreatorComputerName, strDTSID,
+                    strExecutableType, strLastModifiedProductVersion, strLocaleID,
+                    strObjectName, strPackageType, strVersionBuild, strVersionGUID
+                    );
+
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
